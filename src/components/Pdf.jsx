2028bcd482e185be3941566631186c99df48bb32
Martin Liversage <martin@liversage.com>
2023-06-06 21:27:56 +0200

    Marker PDF forhåndsvisning med en ramme

--- src/components/Pdf.jsx
+++ src/components/Pdf.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { useTheme } from '@mui/material/styles';
 import Link from '@mui/material/Link';
 
 export default function Pdf({ pdf, image, text }) {
+    const theme = useTheme();
+    console.log(theme.palette);
+
     // eslint-disable-next-line react/jsx-no-target-blank
     return (
         <Link href={pdf} color="secondary" underline="hover" target="_blank">
-            {image ? <img css={{ width: '100%' }} src={image} alt={text} /> : <span>{text}</span>}
+            {image ? (
+                <img
+                    css={{
+                        width: '100%',
+                        outlineStyle: 'solid',
+                        outlineColor: theme.palette.primary.main,
+                        '&:hover': {
+                            outlineColor: theme.palette.secondary.main,
+                        },
+                    }}
+                    src={image}
+                    alt={text}
+                />
+            ) : (
+                <span>{text}</span>
+            )}
         </Link>
     );
 }
